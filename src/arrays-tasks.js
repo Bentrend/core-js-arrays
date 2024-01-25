ec5319c193dc35fde3df74ca046c66d3193da413
bentrend <trend4309@gmail.com>
2024-01-25 10:36:44 +0300

    feat: compleate 25 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -381,8 +381,17 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const arrFalsy = [null, undefined, NaN, false, 0, ''];
+  return arr.reduce((akk, current) => {
+    let akkum = akk;
+    if (arrFalsy.includes(current)) {
+      akkum += 1;
+      return akkum;
+    }
+    akkum += 0;
+    return akkum;
+  }, 0);
 }
 
 /**
